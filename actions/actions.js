53d3738e802de88f5ef8f98a70fdeb0df53a7e99
Anna <annanarimanyan10@gmail.com>
2018-07-11 17:32:26 +0400

    revised files

--- actions/actions.js
+++ actions/actions.js
@@ -1,34 +1,42 @@
-export const addItemAction = (value, bool) => ({
-  type: 'ADD_ITEM',
-  payload: {
-    value,
-    is_add: bool,
-    type: 'ADD_ITEM'
-  }
-});
-
-export const editItemAction = (value, bool) => ({
-  type: 'EDIT_ITEM',
-  payload: {
-    value,
-    is_add: bool,
-    type: 'EDIT_ITEM'
-  }
-});
-
-export const deleteAction = (index) => ({
-  type: 'DELETE_ITEM',
-  payload: {
-    index,
-    type: 'DELETE_ITEM'
-  }
-});
-
-export const changeToEditAction = (value, index) => ({
-  type: 'CHANGE_TO_EDIT',
-  payload: {
-    value,
-    index,
-    type: 'CHANGE_TO_EDIT'
-  }
+export const addItemAction = (value, bool) => (
+ (dispatch) => dispatch({
+ type: 'ADD_ITEM',
+ payload: {
+   value,
+   is_add: bool,
+   type: 'ADD_ITEM',
+ }
 })
+);
+
+export const editItemAction = (value, bool) => (
+  (dispatch) => dispatch({
+    type: 'EDIT_ITEM',
+    payload: {
+      value,
+      is_add: bool,
+      type: 'EDIT_ITEM'
+    }
+  })
+);
+
+export const deleteAction = (index) => (
+  (dispatch) => dispatch({
+    type: 'DELETE_ITEM',
+    payload: {
+      index,
+      type: 'DELETE_ITEM'
+    }
+  })
+);
+
+export const changeToEditAction = (value, index) => (
+  (dispatch) => dispatch({
+    type: 'CHANGE_TO_EDIT',
+    payload: {
+      value,
+      index,
+      type: 'CHANGE_TO_EDIT'
+    }
+  })
+);
